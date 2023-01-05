e03a3f119087b7f990ac15791e9af5a2355de7ff
kateBea <55351037+kateBea@users.noreply.github.com>
2023-01-05 06:59:22 +0100

    implemented file size

--- src/include/kt_image.h
+++ src/include/kt_image.h
@@ -5,6 +5,8 @@
 #include <cstdio>
 #include <iostream>
 #include <fstream>
+#include <iomanip>
+#include <cstdint>
 
 // just make simple image loader to load
 // the image in the repo for now
@@ -12,9 +14,12 @@
 namespace kate {
 class ImageLoader {
 public:
+    using SizeType = std::size_t;
+    using PathType = std::filesystem::path;
+    using FileType = std::ifstream;
     ImageLoader(std::filesystem::path filePath) 
         :   m_FilePath(filePath), m_FileImage(filePath, std::ios::binary) {
-        if (this->m_FileImage.is_open()) {
+        if (!this->m_FileImage.is_open()) {
             std::cout << "-----------------------------\n";
             std::cout << "Error: Could not open file.\n";
             std::cout << "-----------------------------\n";
@@ -25,17 +30,46 @@ public:
             std::cout << "-----------------------------\n";
         }
 
-        std::wcout << "Path source: [ " << this->m_FilePath.c_str() << " ]\n";
+#ifdef _DEBUG
+        printSomeStuf();
+        std::cout << std::dec << "\nFile size: " << getFileSize() << " bytes\n";
+#endif
     }   
 
     auto getFilePath() const -> const std::filesystem::path& {
         return m_FilePath;
     }
 
+#ifdef _DEBUG
+    auto printSomeStuf() -> void {  
+        std::cout << std::endl;
+        for (int i = 0; i < 10; ++i) {
+            unsigned char byte;
+            this->m_FileImage >> byte;
+            std::cout << std::uppercase << std::hex << static_cast<int>(byte) << ' ';
+        }
+    }
+#endif
+
+    auto getFileSize() -> SizeType {
+        // backup previous position
+        FileType::pos_type previousPosition = this->m_FileImage.tellg();
+
+        // set read position to end of file and infer size
+        this->m_FileImage.seekg(0, std::ios::end);
+        SizeType result = this->m_FileImage.tellg();
+
+        // restore read position to the previous position
+        this->m_FileImage.seekg(previousPosition);
+
+        return result;
+    }
+
 
 private:
-    std::filesystem::path m_FilePath{};
-    std::ifstream m_FileImage{};
+    PathType m_FilePath{};
+    FileType m_FileImage{};
+    SizeType fileSize{};
 
 };  // END ImageLoader class
 
