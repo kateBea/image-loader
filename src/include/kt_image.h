f91b270f315fac64fdfcadc1bbfa2f6b8c000721
kateBea <55351037+kateBea@users.noreply.github.com>
2023-01-05 06:15:19 +0100

    some additions

--- src/include/kt_image.h
+++ src/include/kt_image.h
@@ -1,4 +1,44 @@
 #ifndef KT_IMAGE_HH
 #define KT_IMAGE_HH
 
+#include <filesystem>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+
+// just make simple image loader to load
+// the image in the repo for now
+// i.e. support for .png files
+namespace kate {
+class ImageLoader {
+public:
+    ImageLoader(std::filesystem::path filePath) 
+        :   m_FilePath(filePath), m_FileImage(filePath, std::ios::binary) {
+        if (this->m_FileImage.is_open()) {
+            std::cout << "-----------------------------\n";
+            std::cout << "Error: Could not open file.\n";
+            std::cout << "-----------------------------\n";
+        }
+        else {
+            std::cout << "-----------------------------\n";
+            std::cout << "Succes: Could open file succesfully.\n";
+            std::cout << "-----------------------------\n";
+        }
+
+        std::wcout << "Path source: [ " << this->m_FilePath.c_str() << " ]\n";
+    }   
+
+    auto getFilePath() const -> const std::filesystem::path& {
+        return m_FilePath;
+    }
+
+
+private:
+    std::filesystem::path m_FilePath{};
+    std::ifstream m_FileImage{};
+
+};  // END ImageLoader class
+
+} // END kate NAMESPACE
+
 #endif
--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,12 @@
 #include <kt_image.h>
 #include <iostream>
+#include <string>
 
 int main(int, char**) {
-    std::cout << "This is a test line\n";
+    std::string dir{};
+    std::cout << "Enter image source path: ";
+    std::cin >> dir;
+    kate::ImageLoader img(dir.c_str());
 
     return 0;
 }
